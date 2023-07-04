9c536bdaee3f7240d4069284fe7762cb087d6192
hugo-ravailhe <hugo.rh@free.fr>
2023-07-04 09:27:23 +0200

    Update Main.scala

--- sudoku-project/src/main/scala/Main.scala
+++ sudoku-project/src/main/scala/Main.scala
@@ -1,5 +1,14 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+package sudoku
 
-def msg = "I was compiled by Scala 3. :)"
+import zio._
+
+object Main extends ZIOAppDefault {
+
+  def run: ZIO[Any, Throwable, Unit] =
+    for {
+      _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem:")
+      path <- Console.readLine
+      _ <-  Console.printLine(s"You entered: $path")
+      // Add your Sudoku solver logic here, utilizing ZIO and interacting with the ZIO Console
+    } yield ()
+}
